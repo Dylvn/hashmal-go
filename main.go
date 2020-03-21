4cdd3fe29bf4cb51e052d7c82eec237a9fe3bab0
Dylvn <trochain.dylan@gmail.com>
2020-03-21 17:54:03 +0100

    Create index route, index handler, modify/add templates

--- main.go
+++ main.go
@@ -3,14 +3,27 @@ package main
 import (
 	"net/http"
 
+	"github.com/Dylvn/hashmal-go/config"
 	"github.com/Dylvn/hashmal-go/users"
 )
 
 func main() {
+	// Assets
 	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("assets"))))
+
+	// Users
 	http.HandleFunc("/register", users.Create)
 	http.HandleFunc("/users/process", users.Store)
 	http.HandleFunc("/login", users.Login)
 	http.HandleFunc("/login/process", users.LoginProcess)
+
+	// Offices
+
+	// Index
+	http.HandleFunc("/", index)
 	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	config.Tpl.ExecuteTemplate(w, "index.gohtml", nil)
 }
